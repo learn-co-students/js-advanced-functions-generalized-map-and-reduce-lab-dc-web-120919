ddc6a64bb9e54e17c4f4be44522dca31cf1564a9
John Louis Rumingan <jmrumingan@gmail.com>
2020-02-13 17:25:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, callback){
+    let a = [];
+    for(let i=0;i<array.length;i++){
+        a.push(callback(array[i]));
+    }
+    return a;
+}
+
+function reduce(array, callback, starting=0){
+
+    let r = (!!starting) ? starting : array[0];
+    let i = (!!starting) ? 0 : 1;
+
+    for (i; i<array.length; i++){
+        r = callback(array[i], r)
+    }
+    return r;
+}
